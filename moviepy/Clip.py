91a6c2fb1c2bcdf56eed37bc272e84913173cb3b
tejas25789@gmail.com <tejas25789@gmail.com>
2018-03-17 18:35:48 +0530

    possible bug fix

--- moviepy/Clip.py
+++ moviepy/Clip.py
@@ -357,6 +357,7 @@ class Clip:
     @convert_to_seconds(['t_start', 't_end'])
     @apply_to_mask
     @apply_to_audio
+    @outplace
     def subclip(self, t_start=0, t_end=None):
         """
         Returns a clip playing the content of the current clip
